cb3342f1b2cc149702d05837acc6b7e279b2ac6a
Robert Fox <pv2fox@gmail.com>
2021-06-14 23:14:03 -0700

    started integrating yahoo-finance api into angular stock component

--- JS/PersonalSite/src/app/stock-ticker/stock-ticker.component.ts
+++ JS/PersonalSite/src/app/stock-ticker/stock-ticker.component.ts
@@ -1,8 +1,23 @@
 import { Component } from "@angular/core";
+// var yahooFinance = require('yahoo-finance');
+import * as yahooFinance from 'yahoo-finance';
 @Component({
     selector: 'app-stock-ticker',
     templateUrl: './stock-ticker.component.html'
 })
 export class StockTickerComponent{
+    ngOnInit(){
+        const stock_arr = ['KRFG', 'AMD', 'AMC', 'MVIS', 'NFLX', 'NVDA', 'DODFX', 'FXAIX', 'VSGAX'];
+        for(var i = 0; i < stock_arr.length; i++){
+            // yahooFinance.quote({
+            //     symbol: stock_arr[i],
+            //     modules: ['price']
+            // })
+        }
+    }
+    
 
 }
+// previous day close
+// current price
+// stock percent gain compared to previous day close
